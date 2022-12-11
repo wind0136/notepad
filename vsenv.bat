75a7283cd1a635e003ef34cffbe8d25cd4691e69
Dongli Li <wind0136@hotmail.com>
2022-12-12 02:25:26 +0800

    add python ws

--- vsenv.bat
+++
@@ -1,3 +0,0 @@
-set path=%path%;C:\home\llvm\install\bin
-REM %comspec% /k "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
-cmd /k "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
---
+++ ws/script/pyenv.bat
@@ -0,0 +1,11 @@
+
+rem  A python assistant script
+
+set CWD=%~dp0
+
+set PYTHON_HOME=%CWD%..\bin
+
+set path=%PYTHON_HOME%\Scripts;%PYTHON_HOME%;%path%
+
+
+cmd
